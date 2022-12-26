2aba35e0c3185af48b4964dd8326713e0efab3c4
“Salil48” <salilchharia@gmail.com>
2022-12-26 20:11:50 +0530

    Daily Wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,9 +8,13 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        public static void CheckAttendence()
+        public static void DailyWage()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
@@ -18,11 +22,15 @@ namespace EmployeeWageProblem
             if(empCheck== IS_FULL_TIME) 
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else 
             {
                 Console.WriteLine("Employee is Absent");
+                empWage = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
