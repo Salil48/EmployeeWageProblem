bf42a9a2d5572d9cce7b130222c7eb43f92d8de1
“Salil48” <salilchharia@gmail.com>
2022-12-27 12:54:11 +0530

    Wage using Switch Case

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,33 +8,33 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        public static void PartTime()
+        public static void Case()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
 
+            
             Random random = new Random();
             int empCheck = random.Next(3);
 
-            if(empCheck== IS_FULL_TIME) 
+            switch(empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case 0:
+                    empHrs = 0; 
+                    break;
             }
-            else if(empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part time");
-                empHrs = 4;
-            }
-            else 
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
+
+            
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
         }
