0a7406970626fa2dafb8b6b81b44dfa2a0a668b4
“Salil48” <salilchharia@gmail.com>
2022-12-27 12:13:02 +0530

    Also part Time

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,26 +8,32 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        public static void DailyWage()
+        public static void PartTime()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if(empCheck== IS_FULL_TIME) 
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
             }
+            else if(empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part time");
+                empHrs = 4;
+            }
             else 
             {
                 Console.WriteLine("Employee is Absent");
-                empWage = 0;
+                empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
