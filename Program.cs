43f64163395534826e126319a3187c0f8cd45498
“Salil48” <salilchharia@gmail.com>
2022-12-27 12:15:03 +0530

    Welcome to Employee Wage Computation Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            EmployeeWage.DailyWage();
+            EmployeeWage.PartTime();
         }
     }
 }
