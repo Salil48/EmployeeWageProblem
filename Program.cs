59f3d204e7920d3b4fe84a30c926fccf443d168c
“Salil48” <salilchharia@gmail.com>
2023-01-05 19:27:00 +0530

    Employee Total Wage for multiple companies

--- Program.cs
+++ Program.cs
@@ -11,9 +11,17 @@
             //EmployeeWage employeeWage = new EmployeeWage();
             //employeeWage.CheckWage();
 
-            EmployeeWage obj = new EmployeeWage();
-            obj.Cal_Wage("Dell", 20, 15, 80);
-            obj.Cal_Wage("Apple", 25, 10, 60);
+            //EmployeeWage obj = new EmployeeWage();
+            //obj.Cal_Wage("Dell", 20, 15, 80);
+            //obj.Cal_Wage("Apple", 25, 10, 60);
+
+            EmployeeWage obj = new EmployeeWage("Dell", 20, 20, 80);
+            obj.Cal_Wage();
+            Console.WriteLine(obj.toString());
+
+            EmployeeWage obj1 = new EmployeeWage("Apple", 25, 10, 90);
+            obj1.Cal_Wage();
+            Console.WriteLine(obj1.toString());
 
         }
     }
