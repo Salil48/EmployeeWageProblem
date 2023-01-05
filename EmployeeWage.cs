08ba9e6db7421239518680d9b76bd82192cdd6f5
“Salil48” <salilchharia@gmail.com>
2023-01-05 19:26:49 +0530

    Employee Total Wage for multiple companies

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,7 +8,20 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        public void Cal_Wage(string Company, int wagePerHour, int maxdays, int maxhours)
+        private string Company;
+        private int wagePerHour;
+        private int maxdays;
+        private int maxhours;
+        private int Monthwork;
+        public EmployeeWage(string Company, int wagePerHour, int maxdays, int maxhours)
+        {
+            this.Company = Company;
+            this.wagePerHour = wagePerHour;
+            this.maxdays = maxdays;
+            this.maxhours = maxhours;
+        }
+
+        public void Cal_Wage()
         {
             Console.WriteLine("\n Program for Calculating {0} Employee Wage !!\n", Company);
 
@@ -17,10 +30,10 @@ namespace EmployeeWageProblem
             int emphours = 0;
             int days = 1;
             int Totalwork = 0;
-            int Monthwork = 0;
 
 
-            while ((emphours <= maxhours) && (days <= maxdays))
+
+            while ((emphours <= this.maxhours) && (days <= this.maxdays))
             {
                 Random random = new Random();
                 int atten = random.Next(3);
@@ -28,12 +41,12 @@ namespace EmployeeWageProblem
 
                 if (atten == 1)
                 {
-                    Totalwork = wagePerHour * 8;
+                    Totalwork = this.wagePerHour * 8;
                     workingHours = 8;
                 }
                 else if (atten == 2)
                 {
-                    Totalwork = wagePerHour * 4;
+                    Totalwork = this.wagePerHour * 4;
                     workingHours = 4;
                 }
                 else
@@ -45,14 +58,20 @@ namespace EmployeeWageProblem
 
                 if (emphours <= maxhours)
                 {
-                    Monthwork += Totalwork;
+                    this.Monthwork += Totalwork;
                     Console.WriteLine("day::{0} employee_hour::{1} empwage : {2}", days, emphours, Totalwork);
                 }
                 days++;
             }
-            Console.WriteLine("TOTAL MONTH WORK: " + Monthwork);
+            Console.WriteLine("TOTAL MONTH WORK: " + this.Monthwork);
+        }
 
-
+        public string toString()
+        {
+            return "the company : " + this.Company + " total work is :  " + this.Monthwork;
         }
     }
 }
+
+
+
